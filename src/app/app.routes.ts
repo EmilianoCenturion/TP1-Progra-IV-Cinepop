5e39b49f9846e2bd5726fff2c30b49c293013870
EmilianoCenturion <emilianodavidcenturion@gmail.com>
2026-09-23 05:30:02 -0300

    feat: agregar rutas de cartelera y ficha de pelicula

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,13 +16,22 @@ export const routes: Routes = [
         loadComponent: () => import("./componentes/login/login").then((c) => c.Login)
     },
     {
-        path: "home",
-        loadComponent: () => import("./componentes/home/home").then((c) => c.Home),
-        canActivate: [authGuard]
-    },
-    {
-        path: "cartelera",
-        loadComponent: () => import("./componentes/cartelera/cartelera").then((c) => c.Cartelera),
-        canActivate: [authGuard]
+        path: "",
+        loadComponent: () => import("./componentes/layout/layout").then((c) => c.Layout),
+        canActivate: [authGuard],
+        children: [
+            {
+                path: 'home', loadComponent: () => import("./componentes/home/home").then((c) => c.Home),
+            },
+            {
+                path: 'cartelera', loadComponent: () => import("./componentes/cartelera/cartelera").then((c) => c.Cartelera)
+            },
+            {
+                path: 'pelicula/:id', loadComponent: () => import("./componentes/pelicula/pelicula").then((c) => c.Pelicula)
+            },
+            {
+                path: 'funcion/:id/butacas', loadComponent: () => import("./componentes/butacas/butacas").then((c) => c.Butacas)
+            }
+        ]
     }
-];
+]
